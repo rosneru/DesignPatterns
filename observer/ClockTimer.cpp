7e22b0e9bd572613965d3d9451b88e6ead8834b2
Uwe Rosner <oscottmusic@posteo.de>
2019-10-07 16:40:31 +0200

    Workaround for deprecated localtime function

--- observer/ClockTimer.cpp
+++ observer/ClockTimer.cpp
@@ -41,7 +41,24 @@ void ClockTimer::Tick()
   // Get and store the current time
   system_clock::time_point now = system_clock::now();
   time_t tt = system_clock::to_time_t(now);
-  tm localTm = *localtime(&tt);
+   
+  std::tm localTm {};
+
+  //
+  // Using Windows-/UNIX-specific variants of the potential unsafe 
+  // localtime call. On all other platforms using a mutex when for localtime.
+  //
+
+#if defined(__unix__)
+  localTm = *localtime(&tt, &localTm);
+#elif defined(_MSC_VER)
+  localtime_s(&localTm, &tt);
+#else
+  static std::mutex mtx;
+  std::lock_guard<std::mutex> lock(mtx);
+  localTm = localtime(&timer);
+#endif
+
   m_Hour = localTm.tm_hour;
   m_Minute = localTm.tm_min;
   m_Second = localTm.tm_sec;
