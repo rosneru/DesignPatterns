70714c0e4356d589f37e653e7a5cc7d84b7871c5
Uwe Rosner <oscottmusic@posteo.de>
2025-05-07 15:27:54 +0200

    Added idea to implement `Context` class

--- src/interpreter/Context.hpp
+++ src/interpreter/Context.hpp
@@ -8,6 +8,26 @@ class Context
 public:
   bool Lookup(const char*) const;
   void Assign(VariableExp*, bool);
+
+/* TODO: Change it like this: (??)
+class Context
+{
+public:
+    bool Lookup(const char* name) const {
+        auto it = variables.find(name);
+        return it != variables.end() ? it->second : false; // Standardwert: false
+    }
+
+    void Assign(VariableExp* var, bool value) {
+        // Annahme: VariableExp hat eine Methode, um den Namen abzurufen
+        variables[var->GetName()] = value;
+    }
+
+private:
+    std::map<std::string, bool> variables;
+};
+
+ */
 };
 
 #endif
